3139ca158c5f8672b8167d0558d81b257886203f
kamazgovna <83076680+kamazgovna@users.noreply.github.com>
2021-04-23 20:41:07 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+    <!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>zRealEstate - Free Html5 Templates</title>
+	<meta name="description" content="Free Responsive Html5 Css3 Templates | zerotheme.com">
+	<meta name="author" content="www.zerotheme.com">
+	
+    <!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- CSS
+  ================================================== -->
+  	<link rel="stylesheet" href="css/zerogrid.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsiveslides.css">
+	
+	<!-- Custom Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	<script src="js/jquery-latest.min.js"></script>
+	<script src="js/script.js"></script>
+    <script src="js/jquery183.min.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+    <script>
+		// You can also use "$(window).load(function() {"
+		$(function () {
+		  // Slideshow 
+		  $("#slider").responsiveSlides({
+			auto: true,
+			pager: false,
+			nav: true,
+			speed: 500,
+			namespace: "callbacks",
+			before: function () {
+			  $('.events').append("<li>before event fired.</li>");
+			},
+			after: function () {
+			  $('.events').append("<li>after event fired.</li>");
+			}
+		  });
+		});
+	</script>
+	
+	
+    
+</head>
+<body class="home-page">
+<div class="wrap-body">
+
+<!--////////////////////////////////////Header-->
+<header >
+	<div class="wrap-header zerogrid">
+		<div class="row">
+			<div id="cssmenu">
+				<ul>
+				   <li class='active'><a href="index.html">Home</a></li>
+				   <li><a href="about.html">About</a></li>
+				   <li><a href="projects.html">Projects</a></li>
+				   <li><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+			<a href='index.html' class="logo"><img src="images/logo.png" /></a>
+		</div>
+	</div>
+</header>
+
+
+
+
+		
+		
+		
+		<footer>
+	<div class='embed-container maps' style="margin: 0; padding: 0;">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.289259162295!2d-120.7989351!3d37.5246781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8091042b3386acd7%3A0x3b4a4cedc60363dd!2sMain+St%2C+Denair%2C+CA+95316%2C+Hoa+K%E1%BB%B3!5e0!3m2!1svi!2s!4v1434016649434" width="100%" height="250px" frameborder="0" style="border: 0;"></iframe>
+	</div>
+	<div class="wrap-footer">
+		<div class="zerogrid">
+			<div class="row">
+				<h3>Contact</h3>
+				<span>Phone / +80 999 99 9999 </span></br>
+				<span>Email / info@domain.com  </span></br>
+				<span>Studio / Moonshine St. 14/05 Light City </span></br>
+				<span><strong>Copyright 20xx - Designed by <a href="https://www.zerotheme.com">ZEROTHEME</a></strong></span>
+				<ul class="social-buttons">
+					<li><a href="#"><i class="fa fa-twitter"></i></a>
+					</li>
+					<li><a href="#"><i class="fa fa-facebook"></i></a>
+					</li>
+					<li><a href="#"><i class="fa fa-linkedin"></i></a>
+					</li>
+					<li><a href="#"><i class="fa fa-google-plus"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+
+
+
+
+		<section class="content-box box-3">
+			<div class="zerogrid">
+				<div class="row wrap-box"><!--Start Box-->
+					<div class="row">
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>COMMERCIAL</h3>
+								<span>MES CUML DIA SED INENIAS CET INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>LUXURY</h3>
+								<span>MES CUML DIA  INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>RESIDENTIAL</h3>
+								<span>MES CUML DIA SED INENIAS CET INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<!-----------------content-box-4-------------------->
+		<section class="content-box boxstyle-2 box-4">
+			<div class="zerogrid">
+				<div class="row wrap-box"><!--Start Box-->
+					<div class="header">
+						<div class="wrapper">
+							<h2>FEATURED OFFERS</h2>
+						</div>
+					</div>	
+					<div class="row">
+						<div class="col-1-4">
+							<div class="wrap-col post">
+								<img class="example-image" src="images/kids1-thumb.jpg" alt=""/>
+								<div class="price">$ 75,000</div>
+								<h3>MES CUML DIA SED LOTE DOLORMOD ICTOR.</h3>
+								<p>His primis omittam intellegat cu, voluptua appetere mea ad, eu harum oporteat vix. Et vel quod legimus, graeci electram ocurreret at his. Vix at tation facete impetus omnesque ius harum antiopam.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-4">
+							<div class="wrap-col post">
+								<img class="example-image" src="images/kids2-thumb.jpg" alt=""/>
+								<div class="price">$ 45,000</div>
+								<h3>MES CUML DIA SED LOTE DOLORMOD ICTOR.</h3>
+								<p>His primis omittam intellegat cu, voluptua appetere mea ad, eu harum oporteat vix. Et vel quod legimus, graeci electram ocurreret at his. Vix at tation facete impetus omnesque ius harum antiopam.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-4">
+							<div class="wrap-col post">
+								<img class="example-image" src="images/kids3-thumb.jpg" alt=""/>
+								<div class="price">$ 50,000</div>
+								<h3>MES CUML DIA SED LOTE DOLORMOD ICTOR.</h3>
+								<p>His primis omittam intellegat cu, voluptua appetere mea ad, eu harum oporteat vix. Et vel quod legimus, graeci electram ocurreret at his. Vix at tation facete impetus omnesque ius harum antiopam.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-4">
+							<div class="wrap-col post">
+								<img class="example-image" src="images/kids4-thumb.jpg" alt=""/>
+								<div class="price">$ 25,000</div>
+								<h3>MES CUML DIA SED LOTE DOLORMOD ICTOR.</h3>
+								<p>His primis omittam intellegat cu, voluptua appetere mea ad, eu harum oporteat vix. Vel quod legimus, graeci electram ocurreret at his. Vix at tation facete impetus omnesque ius harum antiopam.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+			<section class="content-box box-3">
+			<div class="zerogrid">
+				<div class="row wrap-box"><!--Start Box-->
+					<div class="row">
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>COMMERCIAL</h3>
+								<span>MES CUML DIA SED INENIAS CET INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>LUXURY</h3>
+								<span>MES CUML DIA SED INENIAS CET INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+						<div class="col-1-3">
+							<div class="wrap-col wrapper">
+								<h3>RESIDENTIAL</h3>
+								<span>MES CUML DIA SED INENIAS CET INGER LOT ALIIQTES</span>
+								<p>Mes cuml dia sed in lacus ut eniascet ingerto aliiqt es site amet eismod ictor ut ligulate ameti dapibus ticdu nt mtsen lusto dolor ltissim. comes cuml dia sed inertio.</p>
+								<a class="button" href="single.html">Learn More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		
+		<!--////////////////////////////////////Container-->
+<section id="container">
+	<div class="wrap-container">
+		<!-----------------content-box-1-------------------->
+		<section class="content-box boxstyle-2 box-1">
+			<div class="zerogrid">
+				<div class="row wrap-box t-center"><!--Start Box-->
+					<h1>Hello There!</h1>
+					<span>WELCOME TO OUR SITE</span>
+					<p>De ezek a számok nem minden korosztály számára azonosak?.</p>
+				</div>
+			</div>
+		</section>
+		
+		<!-----------------content-box-2-------------------->
+		<section class="content-box boxstyle-1 box-2">
+			<div class="zerogrid">
+				<div class="row wrap-box"><!--Start Box-->
+					
+					<span>Simon Matskeplishvili: nem lehet egyetlen a priori. Kívánatos, hogy a 65 évesnél fiatalabb betegek szisztolés ("felső") nyomásértékei 120-129 Hgmm tartományban legyenek. A 65 évesnél idősebb betegeknél a szisztolés nyomásértékeket a 140-150 mm tartományban korábban normának tekintették. Most ezeket a mutatókat 130-139 mm-re kell csökkenteni.ugyanazok a mutatók ajánlottak a 80 évesnél idősebb emberek számára. És még többet. A kezelés eredményeképpen az alacsonyabb nyomás értékeit a betegek minden csoportjában, függetlenül a kockázat mértékétől és az egyidejű betegségektől, nem 90-re kell csökkenteni, ahogy azt korábban gondoltuk, hanem 80-ra vagy annál alacsonyabb értékre.</span>
+				</div>
+			</div>
+		</section>
+		<div class="slider">
+	<!-- Slideshow -->
+	<div class="callbacks_container" >
+		<ul class="rslides" id="slider">
+			<li>
+				<img src="images/slideshow-image1.jpg" alt="">
+				<div class="caption">
+						<span > HATÉKONYAK-E A HAGYOMÁNYOS VÉRNYOMÁSSZABÁLYZÓK? MIT KELL TUDNIA MINDEN MAGAS VÉRNYOMÁSTÓL SZENVEDŐ EMBERNEK?.</span>
+					<div class="price">$ 170,000</div>
+				</div>
+			</li>
+			<li>
+				<img src="images/slideshow-image2.jpg" alt="">
+				<div class="caption">
+					
+					<span > HATÉKONYAK-E A HAGYOMÁNYOS VÉRNYOMÁSSZABÁLYZÓK? MIT KELL TUDNIA MINDEN MAGAS VÉRNYOMÁSTÓL SZENVEDŐ EMBERNEK?.</span>
+					<div class="price">$ 170,000</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="clear"></div>
+</div>
+		<!-----------------content-box-3-------------------->
+		
+		
+		
+	</div>
+</section>
+<form method="post" action="success.php" class="login">
+    <p>
+      <label for="login">name:</label>
+      <input type="text" name="login" id="login" value="name@example.com">
+    </p>
+
+    <p>
+      <label for="password">phone:</label>
+      <input type="password" name="password" id="password" value="4815162342">
+    </p>
+
+    <p class="login-submit">
+      <button type="submit" class="">Contact</button>
+    </p>
+
+   
+  </form>
+
+
+	
+
+	
+</div>
+</body></html>
